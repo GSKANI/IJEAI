f79d63054ec685b0290f900de4166a6cd557564a
thenavya2606 <navyapriyadarsini3@gmail.com>
2026-03-14 01:22:04 +0530

    Remove admin folder completely

--- ijeai-journal/src/admin/Toast.jsx
+++
@@ -1,9 +0,0 @@
-import s from './Admin.module.css'
-
-export default function Toast({ msg, type }) {
-  return (
-    <div className={`${s.toast} ${type === 'err' ? s.toastErr : ''} ${s.toastShow}`}>
-      {msg}
-    </div>
-  )
-}
